ed2feb1023c555cc1f8db24ba378abd18a5c2195
Yixue Zhao <felicitia2010@gmail.com>
2019-07-15 20:24:29 +0100

    update code to have user sessions

--- ShoppingTests/Etsy/welcome.py
+++ ShoppingTests/Etsy/welcome.py
@@ -23,12 +23,8 @@ class WelcomeTests(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_sign_in_wrong(self):
-        # user inputs
-        usrname_input = 'aaa'
-        pwd_input = 'bbb'
-
-        # transition events: go to sign in screen
+    # transition events: fresh start -> sign in screen
+    def transit_to_start(self):
         WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/btn_link')))
         get_started = self.driver.find_element_by_id('com.etsy.android:id/btn_link')
         get_started.click()
@@ -36,6 +32,15 @@ class WelcomeTests(unittest.TestCase):
         sign_in_jump = self.driver.find_element_by_id('com.etsy.android:id/btn_sign_in_dialog')
         sign_in_jump.click()
 
+    # test: sign in empty
+    def sign_in_empty(self):
+        self.sign_in('', '')
+
+    # test: sign in wrong
+    def sign_in_wrong(self):
+        self.sign_in('aaa', 'bbb')
+
+    def sign_in(self, usrname_input, pwd_input):
         # input events: sign in by usrname_input and pwd_input
         WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/edit_username')))
         WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/edit_password')))
@@ -48,32 +53,9 @@ class WelcomeTests(unittest.TestCase):
         
         # assert events: click sign in button
         sign_in.click()
-        # oracle: check if wrong sign in message is displayed
-        self.assertTrue(self.driver.find_element_by_id('com.etsy.android:id/txt_error').is_displayed())
 
-    def test_sign_in_empty(self):
-        # transition events: go to sign in screen
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/btn_link')))
-        get_started = self.driver.find_element_by_id('com.etsy.android:id/btn_link')
-        get_started.click()
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/btn_sign_in_dialog')))
-        sign_in_jump = self.driver.find_element_by_id('com.etsy.android:id/btn_sign_in_dialog')
-        sign_in_jump.click()
-
-        # assert events: click sign in button
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/button_signin')))
-        sign_in = self.driver.find_element_by_id('com.etsy.android:id/button_signin')
-        sign_in.click()
-        # oracle: check empty message
-
-    def test_forget_pwd(self):
-        # transition events: go to sign in screen
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/btn_link')))
-        get_started = self.driver.find_element_by_id('com.etsy.android:id/btn_link')
-        get_started.click()
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/btn_sign_in_dialog')))
-        sign_in_jump = self.driver.find_element_by_id('com.etsy.android:id/btn_sign_in_dialog')
-        sign_in_jump.click()
+    # test: forget password
+    def forget_pwd(self):
 
         # assert events: click forget pwd button
         WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.etsy.android:id/forgot_password_link')))
@@ -81,7 +63,26 @@ class WelcomeTests(unittest.TestCase):
         forget_pwd.click()
 
         # oracle: check if forget pwd dialog is displayed
-        self.assertTrue(self.driver.find_element_by_id('com.etsy.android:id/button_submit_password').is_displayed())
+        # self.assertTrue(self.driver.find_element_by_id('com.etsy.android:id/button_submit_password').is_displayed())
+
+    # user session
+    def test_welcome(self):
+        current_dir = '%s/' % os.getcwd() + 'welcome.'
+
+        self.transit_to_start()
+        # test: sign in empty
+        self.sign_in_empty()
+        screenshot_name = 'sing_in_empty.png'
+        self.driver.save_screenshot(current_dir + screenshot_name)
+        # test: sign in wrong
+        self.sign_in_wrong()
+        screenshot_name = 'sign_in_wrong.png'
+        self.driver.save_screenshot(current_dir + screenshot_name)
+        # test: forget password
+        self.forget_pwd()
+        screenshot_name = 'forget_pwd.png'
+        self.driver.save_screenshot(current_dir + screenshot_name)
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(WelcomeTests)
