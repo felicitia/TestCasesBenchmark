6291613301d02009c4b713c2ddfbf1a0fef4e375
Yixue Zhao <felicitia2010@gmail.com>
2019-07-15 20:51:17 +0100

    add user session

--- ShoppingTests/Wish/welcome.py
+++ ShoppingTests/Wish/welcome.py
@@ -23,41 +23,69 @@ class WelcomeTests(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_sign_in_wrong(self):
-        # user inputs
-        usrname_input = 'aaa'
-        pwd_input = 'bbb'
+    # test: sign in empty
+    def sign_in_empty(self):
+        self.sign_in('', '')
 
+    # test: sign in wrong
+    def sign_in_wrong(self):
+        self.sign_in('aaa', 'bbb')
+
+    def sign_in(self, usrname_input, pwd_input):
         # input events: sign in by usrname_input and pwd_input
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_email_text')))
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_password_text')))
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_sign_in_button')))
+        WebDriverWait(self.driver, time_out).until(
+            EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_email_text')))
+        WebDriverWait(self.driver, time_out).until(
+            EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_password_text')))
+        WebDriverWait(self.driver, time_out).until(
+            EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_sign_in_button')))
         email = self.driver.find_element_by_id('com.contextlogic.wish:id/sign_in_fragment_email_text')
         password = self.driver.find_element_by_id('com.contextlogic.wish:id/sign_in_fragment_password_text')
         sign_in = self.driver.find_element_by_id('com.contextlogic.wish:id/sign_in_fragment_sign_in_button')
         email.send_keys(usrname_input)
         password.send_keys(pwd_input)
-        
+
         # assert events: click sign in button
         sign_in.click()
         # oracle: check if wrong sign in message is displayed
-        self.assertTrue(self.driver.find_element_by_id('com.contextlogic.wish:id/multi_button_dialog_fragment_content').is_displayed())
+        # self.assertTrue(self.driver.find_element_by_id(
+        #     'com.contextlogic.wish:id/multi_button_dialog_fragment_content').is_displayed())
 
-    def test_sign_in_empty(self):
-        # assert events: click sign in button
-        WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_sign_in_button')))
-        sign_in = self.driver.find_element_by_id('com.contextlogic.wish:id/sign_in_fragment_sign_in_button')
-        sign_in.click()
-        # oracle: check empty message
-        self.assertTrue(self.driver.find_element_by_id('com.contextlogic.wish:id/multi_button_dialog_fragment_content').is_displayed())
-
-    def test_forget_pwd(self):
+    # test: forget password
+    def forget_pwd(self):
         # assert events: click forget pwd
         WebDriverWait(self.driver, time_out).until(EC.presence_of_element_located((By.ID, 'com.contextlogic.wish:id/sign_in_fragment_forgot_password_button')))
         forget_pwd = self.driver.find_element_by_id('com.contextlogic.wish:id/sign_in_fragment_forgot_password_button')
         forget_pwd.click()
         # oracle: check if forget pwd dialog is displayed
-        self.assertTrue(self.driver.find_element_by_id('com.contextlogic.wish:id/forgot_password_fragment_reset_password_button').is_displayed())
+        # self.assertTrue(self.driver.find_element_by_id('com.contextlogic.wish:id/forgot_password_fragment_reset_password_button').is_displayed())
+
+    def transit_back(self):
+        WebDriverWait(self.driver, time_out).until(
+            EC.presence_of_element_located((By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.Button')))
+        ok_btn = self.driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.Button')
+        ok_btn.click()
+
+    # user session
+    def test_welcome(self):
+        current_dir = '%s/' % os.getcwd() + 'welcome.'
+
+        # test: sign in empty
+        self.sign_in_empty()
+        screenshot_name = 'sing_in_empty.png'
+        self.driver.save_screenshot(current_dir + screenshot_name)
+        self.transit_back()
+
+        # test: sign in wrong
+        self.sign_in_wrong()
+        screenshot_name = 'sign_in_wrong.png'
+        self.driver.save_screenshot(current_dir + screenshot_name)
+        self.transit_back()
+
+        # test: forget password
+        self.forget_pwd()
+        screenshot_name = 'forget_pwd.png'
+        self.driver.save_screenshot(current_dir + screenshot_name)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(WelcomeTests)
